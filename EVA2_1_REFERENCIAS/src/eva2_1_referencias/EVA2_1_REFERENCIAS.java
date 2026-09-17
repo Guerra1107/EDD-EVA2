9abe61b9a9b0e11d9e3f25bb40a2488cb24442e1
Guerra1107 <L25550289@chihuahua2.tecnm.mx>
2026-09-17 17:48:41 -0600

    Add files via upload

---
+++ EVA2_1_REFERENCIAS/src/eva2_1_referencias/EVA2_1_REFERENCIAS.java
@@ -0,0 +1,33 @@
+
+package eva2_1_referencias;
+
+public class EVA2_1_REFERENCIAS {
+    
+    public static void main(String[] args) { 
+         Ejemplo ejemplo1 = new Ejemplo();
+     System.out.println("ejemplo1 = " + ejemplo1);
+          System.out.println("ejemplo1.val = " + ejemplo1.val);
+                System.out.println("ejemplo1.otro = " + ejemplo1.otro);
+                
+        ejemplo1.otro = new Ejemplo();
+    System.out.println("ejemplo1 = " + ejemplo1.otro);
+        System.out.println("ejemplo1.val = " + ejemplo1.otro.val);
+            System.out.println("ejemplo1.otro = " + ejemplo1.otro.otro);
+            
+            ejemplo1.otro.otro = new Ejemplo();
+            ejemplo1.otro.otro.otro = new Ejemplo();
+            ejemplo1.otro.otro.otro.otro = new Ejemplo ();
+            
+            Ejemplo siguiente = ejemplo1;
+            while(siguiente != null){
+        System.out.println ("[" + siguiente.val + "]");
+        siguiente = siguiente.otro;
+            System.out.println ("");
+        }
+    } 
+}
+
+ class Ejemplo{
+    int val = 5;
+    Ejemplo otro;
+ }
